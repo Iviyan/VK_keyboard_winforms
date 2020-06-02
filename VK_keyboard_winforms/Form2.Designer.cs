cbc7156ece8557146dd9bd3f906c684450eb6cc0
Zagadochnik37 <Elephant-5@yandex.ru>
2020-06-02 10:26:08 +0300

    Добавьте файлы проекта.

---
+++ VK_keyboard_winforms/Form2.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace VK_keyboard_winforms
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.проектToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.CBinline = new System.Windows.Forms.CheckBox();
+            this.CBonetime = new System.Windows.Forms.CheckBox();
+            this.buttonType = new System.Windows.Forms.ComboBox();
+            this.view = new System.Windows.Forms.TabControl();
+            this.previewTP = new System.Windows.Forms.TabPage();
+            this.preview = new System.Windows.Forms.TableLayoutPanel();
+            this.PV_addv = new System.Windows.Forms.Button();
+            this.jsonview = new System.Windows.Forms.TabPage();
+            this.JSONtb = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.view.SuspendLayout();
+            this.previewTP.SuspendLayout();
+            this.preview.SuspendLayout();
+            this.jsonview.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.проектToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.создатьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // проектToolStripMenuItem
+            // 
+            this.проектToolStripMenuItem.Name = "проектToolStripMenuItem";
+            this.проектToolStripMenuItem.Size = new System.Drawing.Size(119, 29);
+            this.проектToolStripMenuItem.Text = "Сохранить";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(99, 29);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(95, 29);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 33);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.view);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 417);
+            this.splitContainer1.SplitterDistance = 330;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.CBinline, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CBonetime, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonType, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(330, 417);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // CBinline
+            // 
+            this.CBinline.AutoSize = true;
+            this.CBinline.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CBinline.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CBinline.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CBinline.Location = new System.Drawing.Point(7, 7);
+            this.CBinline.Name = "CBinline";
+            this.CBinline.Size = new System.Drawing.Size(316, 24);
+            this.CBinline.TabIndex = 0;
+            this.CBinline.Text = "inline";
+            this.CBinline.UseVisualStyleBackColor = true;
+            this.CBinline.CheckedChanged += new System.EventHandler(this.CBinline_CheckedChanged);
+            // 
+            // CBonetime
+            // 
+            this.CBonetime.AutoSize = true;
+            this.CBonetime.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CBonetime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CBonetime.Location = new System.Drawing.Point(7, 38);
+            this.CBonetime.Name = "CBonetime";
+            this.CBonetime.Size = new System.Drawing.Size(316, 24);
+            this.CBonetime.TabIndex = 1;
+            this.CBonetime.Text = "Скрывать клавиатуру сразу";
+            this.CBonetime.UseVisualStyleBackColor = true;
+            this.CBonetime.CheckedChanged += new System.EventHandler(this.CBonetime_CheckedChanged);
+            // 
+            // buttonType
+            // 
+            this.buttonType.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonType.FormattingEnabled = true;
+            this.buttonType.Items.AddRange(new object[] {
+            "Text",
+            "Open Link",
+            "Location",
+            "VK Pay",
+            "VK Apps"});
+            this.buttonType.Location = new System.Drawing.Point(7, 69);
+            this.buttonType.Name = "buttonType";
+            this.buttonType.Size = new System.Drawing.Size(316, 28);
+            this.buttonType.TabIndex = 2;
+            // 
+            // view
+            // 
+            this.view.Controls.Add(this.previewTP);
+            this.view.Controls.Add(this.jsonview);
+            this.view.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.view.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.view.Location = new System.Drawing.Point(0, 0);
+            this.view.Multiline = true;
+            this.view.Name = "view";
+            this.view.SelectedIndex = 0;
+            this.view.Size = new System.Drawing.Size(466, 417);
+            this.view.TabIndex = 0;
+            // 
+            // previewTP
+            // 
+            this.previewTP.Controls.Add(this.preview);
+            this.previewTP.Location = new System.Drawing.Point(4, 29);
+            this.previewTP.Name = "previewTP";
+            this.previewTP.Padding = new System.Windows.Forms.Padding(3);
+            this.previewTP.Size = new System.Drawing.Size(458, 384);
+            this.previewTP.TabIndex = 0;
+            this.previewTP.Text = "preview";
+            this.previewTP.UseVisualStyleBackColor = true;
+            // 
+            // preview
+            // 
+            this.preview.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.preview.ColumnCount = 1;
+            this.preview.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.preview.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.preview.Controls.Add(this.PV_addv, 0, 0);
+            this.preview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.preview.Location = new System.Drawing.Point(3, 3);
+            this.preview.Name = "preview";
+            this.preview.RowCount = 1;
+            this.preview.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 377F));
+            this.preview.Size = new System.Drawing.Size(452, 378);
+            this.preview.TabIndex = 2;
+            // 
+            // PV_addv
+            // 
+            this.PV_addv.AutoSize = true;
+            this.PV_addv.BackColor = System.Drawing.Color.LightGray;
+            this.PV_addv.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PV_addv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PV_addv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PV_addv.Location = new System.Drawing.Point(4, 4);
+            this.PV_addv.Name = "PV_addv";
+            this.PV_addv.Size = new System.Drawing.Size(450, 30);
+            this.PV_addv.TabIndex = 0;
+            this.PV_addv.Text = "+";
+            this.PV_addv.UseVisualStyleBackColor = false;
+            this.PV_addv.Click += new System.EventHandler(this.PV_addv_Click);
+            // 
+            // jsonview
+            // 
+            this.jsonview.Controls.Add(this.JSONtb);
+            this.jsonview.Location = new System.Drawing.Point(4, 29);
+            this.jsonview.Name = "jsonview";
+            this.jsonview.Padding = new System.Windows.Forms.Padding(3);
+            this.jsonview.Size = new System.Drawing.Size(458, 384);
+            this.jsonview.TabIndex = 1;
+            this.jsonview.Text = "JSON";
+            this.jsonview.UseVisualStyleBackColor = true;
+            // 
+            // JSONtb
+            // 
+            this.JSONtb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.JSONtb.Location = new System.Drawing.Point(3, 3);
+            this.JSONtb.Name = "JSONtb";
+            this.JSONtb.Size = new System.Drawing.Size(452, 378);
+            this.JSONtb.TabIndex = 0;
+            this.JSONtb.Text = "";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "Клавиатура";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.view.ResumeLayout(false);
+            this.previewTP.ResumeLayout(false);
+            this.preview.ResumeLayout(false);
+            this.preview.PerformLayout();
+            this.jsonview.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem проектToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TabControl view;
+        private System.Windows.Forms.TabPage previewTP;
+        private System.Windows.Forms.TabPage jsonview;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.CheckBox CBonetime;
+        private System.Windows.Forms.CheckBox CBinline;
+        private System.Windows.Forms.TableLayoutPanel preview;
+        private System.Windows.Forms.ComboBox buttonType;
+        private System.Windows.Forms.RichTextBox JSONtb;
+        private System.Windows.Forms.Button PV_addv;
+    }
+}
---
+++ VK_keyboard_winforms/Form2.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace VK_keyboard_winforms
+{
+    public partial class Form2 : Form
+    {
+        Keyboard keyboard;
+        public Form2()
+        {
+            InitializeComponent(); 
+            //string s = @"{""inline"":false,""one_time"":false,""buttons"":[[{""type"":""text"",""payload"":null}]]}";
+            //keyboard = JsonConvert.DeserializeObject<Keyboard>(s);
+
+            keyboard = new Keyboard();
+            kbUpd();
+        }
+
+        public static class colors
+        {
+            public static Color primary = Color.FromArgb(81, 129, 184);
+            public static Color secondary = Color.White;
+            public static Color negative = Color.FromArgb(230, 70, 70);
+            public static Color positive = Color.FromArgb(75, 179, 75);
+        };
+        class Keyboard
+        {
+            public bool inline { get; set; }
+            public bool one_time { get; set; }
+            public List<List<object>> buttons { get; set; } = new List<List<object>>();
+
+            public class button
+            {
+                abstract public class Action
+                {
+                    public string type { get; set; }
+                    public string payload { get; set; } = "";
+                }
+                public class text : Action
+                {
+                    public text() { type = "text"; }
+                    public string label { get; set; } = "label";
+                }
+                public class open_link : Action
+                {
+                    public open_link() { type = "open_link"; }
+                    public string link { get; set; } = "https://vk.com";
+                    public string label { get; set; } = "label";
+                }
+                public class location : Action
+                {
+                    public location() { type = "location"; }
+
+                }
+                public class vkpay : Action
+                {
+                    public vkpay() { type = "vkpay"; }
+                    public string hash { get; set; } = "";
+                }
+                public class open_app : Action
+                {
+                    public open_app() { type = "open_app"; }
+                    public int app_id { get; set; } = 0;
+                    public int owner_id { get; set; } = 0;
+                    public string hash { get; set; } = "";
+                    public string label { get; set; } = "label";
+                }
+
+                //public Action action;
+                [DefaultValue("primary")]
+                public string color { get; set; } = "primary";
+            }
+            public class text : button
+            {
+                public button.text action { get; set; } = new button.text();
+            }
+            public class open_link : button
+            {
+                public button.open_link action { get; set; } = new button.open_link();
+            }
+            public class location : button
+            {
+                public button.location action { get; set; } = new button.location();
+            }
+            public class vkpay : button
+            {
+                public button.vkpay action { get; set; } = new button.vkpay();
+            }
+            public class open_app : button
+            {
+                public button.open_app action { get; set; } = new button.open_app();
+            }
+
+        }
+
+        public string kbJSON()
+        {
+            JsonSerializerSettings js = new JsonSerializerSettings();
+            js.Formatting = Formatting.Indented;
+            js.DefaultValueHandling = DefaultValueHandling.Ignore;
+            js.NullValueHandling = NullValueHandling.Ignore;
+
+            return JsonConvert.SerializeObject(keyboard,js);
+        }
+
+        public void kbUpd()
+        {
+            JSONtb.Text = kbJSON();
+        }
+
+        private void CBinline_CheckedChanged(object sender, EventArgs e)
+        {
+            bool val = CBinline.Checked;
+            keyboard.inline = val;
+            if (val) CBonetime.Checked = false;
+            kbUpd();
+        }
+
+        private void CBonetime_CheckedChanged(object sender, EventArgs e)
+        {
+            bool val = CBonetime.Checked;
+            keyboard.one_time = val;
+            if (val) CBinline.Checked = false;
+            kbUpd();
+        }
+
+        private void PV_addv_Click(object sender, EventArgs e)
+        {
+            var rc = keyboard.buttons.Count; //rowsCount
+            List<object> l = new List<object>();
+            l.Add(new Keyboard.text());
+            keyboard.buttons.Add(l);
+            kbUpd();
+
+            preview.RowCount++;
+            preview.RowStyles.Insert(rc, new RowStyle(SizeType.Absolute, 38));
+            preview.Controls.Add(PV_addv, 0, rc + 1);
+            if (rc >= 10 - 1)
+            {
+                //preview.RowStyles[rc + 1].Height = 0;
+                PV_addv.Visible = false;
+            }
+
+            TableLayoutPanel row = new TableLayoutPanel();
+            row.Dock = DockStyle.Fill;
+            row.Name = "PV_row_" + (preview.RowCount - 1).ToString();
+            row.ColumnCount = 2;
+            row.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            row.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 30));
+            row.RowCount = 1;
+            row.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            row.Margin = new Padding(0);
+
+            Button PV_btn = new Button();
+            PV_btn.Name = "PV_" + rc.ToString() + "_0";
+            PV_btn.Text = PV_btn.Name;
+            PV_btn.Dock = DockStyle.Fill;
+            PV_btn.BackColor = colors.primary;
+            PV_btn.Margin = new Padding(1);
+            PV_btn.ForeColor = Color.White;
+
+            row.Controls.Add(PV_btn, 0, 0);
+            preview.Controls.Add(row, 0, rc);
+
+            Button PV_addh = new Button();
+            PV_addh.Name = "PV_addh_" + rc.ToString();
+            PV_addh.Text = "+";
+            PV_addh.Dock = DockStyle.Fill;
+            PV_addh.Margin = new Padding(2, 3, 8, 3);
+            row.Controls.Add(PV_addh, 1, 0);
+            PV_addh.Click += PV_addh_Click;
+        }
+
+        private void PV_addh_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int row = int.Parse(btn.Name.Substring("PV_addh_".Length));
+            int rc = keyboard.buttons.Count; //rowsCount
+            int cc = keyboard.buttons[row].Count;
+            keyboard.buttons[row].Add(new Keyboard.text());
+            kbUpd();
+
+            TableLayoutPanel rowel = preview.Controls.Find("PV_row_" + row.ToString(), false).FirstOrDefault() as TableLayoutPanel;
+
+            rowel.ColumnCount++;
+            rowel.ColumnStyles.Insert(cc, new ColumnStyle(SizeType.Percent, 100 / cc));
+            for (int i = 0; i <= rowel.ColumnStyles.Count - 2; i++)
+            {
+                rowel.ColumnStyles[i].SizeType = SizeType.Percent;
+                rowel.ColumnStyles[i].Width = 100 / cc;
+            }
+            
+            rowel.Controls.Add(sender as Button, 0, cc + 1);
+            if (cc >= 5 - 1)
+            {
+                //preview.RowStyles[rc + 1].Height = 0;
+                (sender as Button).Visible = false;
+            }
+
+            Button PV_btn = new Button();
+            PV_btn.Name = "PV_" + rc.ToString() + "_" + cc.ToString();
+            PV_btn.Text = PV_btn.Name;
+            PV_btn.Dock = DockStyle.Fill;
+            PV_btn.BackColor = colors.primary;
+            PV_btn.Margin = new Padding(1);
+            PV_btn.ForeColor = Color.White;
+            rowel.Controls.Add(PV_btn, 0, rc);
+        }
+    }
+}
